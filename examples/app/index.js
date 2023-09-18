ce2047e65a879db6fbfd30916a32d11eb25d190d
XIE <2580924475@qq.com>
2023-09-18 14:32:12 +0800

    fix: change setting.json

--- examples/app/index.js
+++ examples/app/index.js
@@ -1,2 +1,22 @@
+let a = 1;
+console.log(123);
+
+let b = 1;
+
+let c = 1;
+
+let f = 1;
+
+let avc = 1;
+
+let g = 1;
+
+let d = 2;
+
+let fff = 1;
+
+let gg = 1;
+
 var a = 1;
-console.log(a);
+
+let x = 1;
